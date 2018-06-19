57e3fd7988b17de07b1be912068cbbdffd17cb31
YashKandalkar <yashsk.yk@gmail.com>
2018-06-19 21:50:21 +0530

    new files

---
+++ app.js
@@ -0,0 +1,19 @@
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+
+app.get('/', function (request, result) {
+	result.sendfile('index.html');
+});
+
+
+//when a user joins this func executes
+io.on('connection', function (socket) {
+	socket.on('clientEvent', function(data) {
+		console.log(data)
+	});
+});
+
+http.listen(3000, function (){
+	console.log('listening on *:3000');
+});
